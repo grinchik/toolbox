7a66441bf6267ddfe47017c4f65f5d611ebfba16
Victor Grinchik <victor.grinchik@gmail.com>
2025-04-02 19:14:30 +0300

    Removed vscode custom.css support

--- README.md
+++ README.md
@@ -2,14 +2,4 @@
 
 ## VScode Initial Setup
 
-0. Download and install **SF Mono** font: https://developer.apple.com/fonts/
-0. Download and install **PT Mono** font: https://www.paratype.com/fonts/pt/pt-mono
 0. Run `cd vscode && sh install.sh;`
-
-
-## VScode Repair After Update
-
-VScode update reverts the installed UI customization. In order to fix this:
-
-0. Run `cd vscode && sh install.sh;` (ignore `ln <...>: File exists` error)
-0. Restart VScode
--- vscode/install.sh
+++ vscode/install.sh
@@ -4,17 +4,7 @@ set -e;
 set -x;
 
 ln \
-    "./custom.css" \
-    "/Applications/Visual Studio Code.app/Contents/Resources/app/out/vs/code/electron-sandbox/workbench/custom.css" \
-    ;
-
-echo \
-    '<link rel="stylesheet" href="custom.css" />' \
-        >> \
-            "/Applications/Visual Studio Code.app/Contents/Resources/app/out/vs/code/electron-sandbox/workbench/workbench.html" \
-    ;
-
-ln \
-    "./settings.json" \
+    -s \
+    "$(pwd)/settings.json" \
     "${HOME}/Library/Application Support/Code/User/settings.json" \
     ;
